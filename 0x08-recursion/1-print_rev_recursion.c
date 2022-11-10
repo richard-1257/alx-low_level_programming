ce684af804aad964125cf60cae31d012d7bca4a1
richard <richardsifeanyi@gmail.com>
2022-11-09 16:22:41 -0800

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts_recursion - Prints a string, followed by a new line
+ *
+ * @s: string
+ *
+ * Return: no return
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
