7b685e233a922356c47c9dd5edf90a0376ca2c40
richard <richardsifeanyi@gmail.com>
2023-01-17 14:10:10 -0800

    update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,23 +1,29 @@
 #include "lists.h"
 
 /**
- * print_distint - a function that prints all the element of a doubly
- * linked list
- * @h: the head of the linked list
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h: pointer to the head of the list
  *
- * Return: returns the number of node
+ * Return: number of nodes
  */
-size_t print_dlistint(const dlistint_t *h);
-{
-	const dlistint_t *tmp = h;
-	size_t i = 0;
 
-	while (tmp)
+size_t print_dlistint(const dlistint_t *h)
+{
+
+	int counter = 0;
+
+	if (h == NULL)
+		return (counter);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
 	{
-		printf("%d\n", tmp->n);
-		tmp = tmp->next;
-		i++;
+		printf("%d\n", h->n);
+		h = h->next;
+		counter++;
 	}
 
-	return (i);
+	return (counter);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_
+#define _LISTS_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,16 +11,15 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- *
+ * 
  */
 typedef struct dlistint_s
 {
-	int n;
-	struct dlistint_s *prev;
-	struct dlistint_s *next;
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
 } dlistint_t;
 
-/* Function Prototypes */
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
@@ -30,5 +29,4 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-
-#endif /* LISTS_H */
+#endif 
